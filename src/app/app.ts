88c2c328a5daec4cc4f67fb2d105e5e866a042c4
Juan Ortega <juanddog99@gmail.com>
2025-10-31 18:02:53 -0600

    estructura de carpetas y maquetacion de la app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenu } from './shared/components/side-menu/side-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'reactive-forms';
