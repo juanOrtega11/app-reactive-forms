3c94822fded9513e3f9d80eef1d3ccba82993334
Juan Ortega <juanddog99@gmail.com>
2025-11-05 07:38:00 -0600

    separacion de logica a una clase utilitaria

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -7,6 +7,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -16,8 +17,9 @@ import {
 export class BasicPage {
   // Nueva forma con fornm builder
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     // name: ['', /* Validadores síncronos */, /** Validadores Asíncronos */],
     name: ['', [Validators.minLength(3)]],
     price: [0, [Validators.min(10)]],
@@ -31,4 +33,43 @@ export class BasicPage {
     price: new FormControl(0),
     inStorage: new FormControl(0),
   });*/
+
+  /*
+  isValidField(fieldName: string): boolean | null {
+    return (
+      !!this.myForm.controls[fieldName].errors &&
+      !!this.myForm.controls[fieldName].errors
+    );
+  }
+  */
+
+  // getFielError(fieldName: string): string | null {
+  //   if (!this.myForm.controls[fieldName]) return null;
+
+  //   const errors = this.myForm.controls[fieldName].errors ?? {};
+  //   for (const key of Object.keys(errors)) {
+  //     switch (key) {
+  //       case 'required':
+  //         return 'Este campo es requerido';
+
+  //       case 'minlength':
+  //         return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
+
+  //       case 'min':
+  //         return `Valor mínimo de ${errors['min'].min}`;
+  //     }
+  //   }
+  //   return null;
+  // }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
 }
